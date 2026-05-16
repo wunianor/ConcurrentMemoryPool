59e16fef292aada9d031d5e15ddf54395e6302c4
wunianor <156208295+wunianor@users.noreply.github.com>
2026-05-16 11:58:03 +0800

    transcode it to utf8

--- SystemMemoryAlloc.h
+++ SystemMemoryAlloc.h
@@ -1,26 +1,27 @@
 #pragma once
 
+
 #include "Common.h"
 
 /// <summary>
-/// ��ϵͳ����һ���ڴ�
+/// 向系统申请一块内存
 /// </summary>
-/// <param name="size">�ڴ��С</param>
+/// <param name="size">内存大小</param>
 /// <returns>
-/// ����ɹ�,�����ڴ���ʼ��ַ;
-/// ����ʧ��,����nullptr
+/// 申请成功,返回内存起始地址;
+/// 申请失败,返回nullptr
 /// </returns>
 void* systemMemoryAlloc(size_t size);
 
 
 
 /// <summary>
-/// �ͷ��� systemMemoryAlloc() ������ڴ�
+/// 释放由 systemMemoryAlloc() 申请的内存
 /// </summary>
-/// <param name="ptr">��Ҫ�ͷŵ��ڴ�����ʼ��ַ</param>
-/// <param name="size">�ڴ��Ĵ�С</param>
+/// <param name="ptr">需要释放的内存块的起始地址</param>
+/// <param name="size">内存块的大小</param>
 /// <returns>
-/// �ͷųɹ����� true(��ָ���ͷ���Ϊ�ɹ�);
-/// �ͷ�ʧ�ܷ��� false
+/// 释放成功返回 true(空指针释放视为成功);
+/// 释放失败返回 false
 /// </returns>
 bool systemMemoryFree(void* ptr, size_t size);
